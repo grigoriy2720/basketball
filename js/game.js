c8ec527a8f3509a9c39b2c61f5fd7429b383023d
grigoriy2720 <grigoriy2720@mail.ru>
2021-04-05 10:10:25 +0500

    add player and player movement

--- js/game.js
+++ js/game.js
@@ -9,6 +9,21 @@ function doIt() {
       clearInterval(idInt);
       document.getElementById("Timer").innerHTML = "Игра окончена";
     }
+      // console.log(playerJ.style.left);
     i = i - 1;
   }, 1000);
 }
+playerJ = document.getElementById("player");
+x = document.getElementById("player").offsetLeft;
+let xMove = x;
+// playerJ.style.left = 300 + "px";
+document.addEventListener('keydown', function(event){
+  if (['A','a'].includes(event.key)) {
+    xMove -= 10;
+    playerJ.style.left = xMove + "px";
+  }
+  if (['D', 'd'].includes(event.key)) {
+    xMove += 10;
+    playerJ.style.left = xMove + "px";
+  }
+})
